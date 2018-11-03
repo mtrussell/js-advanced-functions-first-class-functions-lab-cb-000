b4766337c94a1b885c1aaa4b05d560fd784a1a7d
Matt Trussell <smtrussell@gmail.com>
2018-11-03 16:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers() {
+  
+}
+
+
+function returnLastTwoDrivers() {
+  
+}
+
+
+const selectingDrivers = [];
+
+
+function createFareMultiplier() {
+  
+}
+
+
+function fareDoubler() {
+  
+}
+
+
+function fareTripler() {
+  
+}
+
+
+function fetchSpecifiedDrivers(arrayOfDrivers, function) {
+  
+}
